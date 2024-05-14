e08a4a68a778567ef75bb59464bff297bc48121d
Rumen Neshev <rstar2@abv.bg>
2024-05-14 23:06:48 +0300

    asd

--- async-result/stressor.js
+++ async-result/stressor.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const fs = require('fs')
-const util = require('util')
 const stream = require('stream')
 const { pipeline, finished } = require('stream/promises')
 const path = require('path')
